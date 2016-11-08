f8cabcd146ca6868837d211a88f6c32287f40678
Teila <inferno256@gmail.com>
2016-11-07 16:42:01 -0800

    Comments updated in code

--- ArcLight_Comms/ArcLight_Comms.ino
+++ ArcLight_Comms/ArcLight_Comms.ino
@@ -1,67 +1,67 @@
-#include "FastLED.h"
-#include "string.h"
+#include <FastLED.h>
+#include <string.h>
 
-#define NUM_LEDS 10
-#define DATA_PIN 6
+#define NUM_LEDS 10     //number of total leds in strip
+#define DATA_PIN 6      //control pin
 
-String str2 = "$";
-String str3 = "*";
-String str[37];
+String str2 = "$";      //start data flag
+String str3 = "*";      //end data flag
+String str[37];     //set expected string length
 
 int counter;
 int x;
 int y;
-int arrayData[3][4];
+int arrayData[3][4];        //array to be built for ((r1,g1,b1,a1)(r2,b2,g2,a2)(w,null,null,null))
 
 CRGB leds[NUM_LEDS];
 
 void setup() {
     Serial.begin(115200);
-    Serial.flush();
+    Serial.flush();     //flush comms
     counter = 0;
-    FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
+    FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);        //setup strip controls
 }
 
 void loop() {
-    while (Serial.available() > 0) {
-        Serial.println("DATA:");            //status
+    while (Serial.available() > 0) {        // wait til data comes in over serial
+//        Serial.println("DATA:");            // Verify data detection
         String str = Serial.readString();
-        Serial.println(str);                //status
-        if (str.startsWith("$")) { //if str starts with $
-            if (str.endsWith("*")) { //and ends with *
+//        Serial.println(str);                // Verify Data recieved
+        if (str.startsWith("$")) {      //if str starts with $
+            if (str.endsWith("*")) {        //and ends with *
                 str.remove(0, 1);
                 str.remove(35);
-                Serial.println("stripped:");  //status
-                Serial.println(str);          //status
-                parseData(str); // call parse code here?
+//                Serial.println("stripped:");      //status 
+//                Serial.println(str);      //Debug stripped data
+                parseData(str);     // call parse code here
             }
         }
-        delay (1000);
+        delay (200);        //cycle delay
     }
-    fill_solid( &(leds[0]), arrayData[2][0] /*number of leds*/, CRGB(arrayData[0][0],arrayData[0][1],arrayData[0][2]) );
-    fill_solid( &(leds[arrayData[2][0]]), NUM_LEDS - arrayData[2][0] /*number of leds*/, CRGB(arrayData[1][0],arrayData[1][1],arrayData[1][2]) );
-    FastLED.show();
+    fill_solid( &(leds[0]), arrayData[2][0] /*number of leds*/, CRGB(arrayData[0][0],arrayData[0][1],arrayData[0][2]) );        //main LEDs
+    fill_solid( &(leds[arrayData[2][0]]), NUM_LEDS - arrayData[2][0] /*number of leds*/, CRGB(arrayData[1][0],arrayData[1][1],arrayData[1][2]) );       //ambiant LEDs
+    FastLED.show();     // start show
     delay (500);
 }
 void parseData(String com) {
     char com_new[com.length() + 1];
     com.toCharArray(com_new, sizeof(com_new));
     char *split = strtok (com_new, ",");
-    int splitdata[9];
+    int splitdata[9];       //total split values
     int cnt1 = 0;
     while (split != NULL) {
         splitdata[cnt1] = atoi(split);        
-            //Serial.println(cnt1);  // 0 1 2 3 4 5 6 7 8 0 1 2
+            //Serial.println(cnt1);     // debug steps
         cnt1++;
         split = strtok(NULL,",");
     }
     int cnt2 = 0;
     for (x = 0; x < 3; x++) {
-      //Serial.println(x);    // 255 255 255 255 255 1 128 128 128 255 255 255
+//      Serial.println(x);      // debug steps
         if (x < 2) {
             for (y = 0; y < 4; y++) {
                 arrayData[x][y] = splitdata[cnt2];
-                    //Serial.println(cnt2);  //
+//                  Serial.println(cnt2);       // debug steps
                 cnt2++;
             }
         } else {
@@ -70,4 +70,3 @@ void parseData(String com) {
     }
 }
 //$045,000,000,255,110,090,045,255,001* example string data
-//int r1 = Serial.parseInt();
